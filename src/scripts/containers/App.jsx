d865ed77747754a60bf894df7e4d54da4b10cc8c
market <market@rrcs-67-78-51-76.sw.biz.rr.com>
2018-01-20 17:11:56 -0600

    Add questions and timer.

--- src/scripts/containers/App.jsx
+++ src/scripts/containers/App.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isPaused: true,
+            secondsRemaining: 60
+        };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        const { isPaused, secondsRemaining } = this.state;
+
+        this.setState({
+            secondsRemaining: !isPaused ? secondsRemaining - 1 : secondsRemaining
+        });
+
+        if (secondsRemaining <= 1) {
+            clearInterval(this.timerID);
+        }
+    }
+
     render() {
+        const { secondsRemaining } = this.state;
+
         return (
             <div>
-            	Hi
+                <div className="count-down-timer">{secondsRemaining}</div>
             </div>
         );
     }
