3ef536e4ef02a1cd66fffd5ad907f9b74f2fb901
Alfredo Yebra <fred.yebra@gmail.com>
2018-12-07 16:43:42 -0600

    Delete existing for refactor.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './styles/main.scss';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from "./scripts/containers/App.jsx";
-
-ReactDOM.render(<App/>, document.getElementById('app'));
--- src/scripts/components/Letter.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Letter = ({ letter }) => {
-    return <div className="letter">{letter}</div>;
-};
-
-export default Letter;
